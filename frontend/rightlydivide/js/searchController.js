737cc85109872cd08a3f3e652b13cd913ac1c17b
deltacypert <dylancypert.work@gmail.com>
2025-01-12 15:19:52 -0600

    Added animate feature and filter-by-type functionality

--- frontend/rightlydivide/js/searchController.js
+++ frontend/rightlydivide/js/searchController.js
@@ -2,6 +2,8 @@
     var rdapp = angular.module('rdapp');
 
     rdapp.controller('searchController', function($scope, $http, $location) {
+
+        $scope.types = ['Literal','Moral','Spiritual'];
     
         $scope.showSpinner = true;
 
@@ -16,7 +18,7 @@
             }
         }
 
-        $scope.getAllObservations(); // calls the function automatically
+        $scope.getAllObservations();
 
         $scope.goToUpdateView = function(observationId) {
             $location.path('/update/' + observationId)
@@ -28,13 +30,37 @@
             if ($scope.observations) {
                 $scope.observations = $scope.observations.slice().sort(function(a, b) {
                     return $scope.reverse
-                        ? new Date(a.createDateTime) - new Date(b.createDateTime) // Ascending
-                        : new Date(b.createDateTime) - new Date(a.createDateTime); // Descending
+                        ? new Date(a.createDateTime) - new Date(b.createDateTime)
+                        : new Date(b.createDateTime) - new Date(a.createDateTime);
                 });
-                $scope.reverse = !$scope.reverse; // Toggle sorting direction
+                $scope.reverse = !$scope.reverse;
             }
         };
 
+        $scope.selectedType = '';
+
+        $scope.selectType = function(type) {
+            $scope.selectedType = type;
+            $scope.filterByType();
+          };
+
+          $scope.filterByType = function() {
+            $scope.showSpinner = true;
+      
+            if ($scope.selectedType) {
+              $http.get("http://localhost:8080/api/observations/type/" + $scope.selectedType)
+                .then(function(response) {
+                  $scope.observations = response.data;
+                  $scope.showSpinner = false;
+                })
+                .catch(function(error) {
+                  console.log("Error HTTP GET by type: ", error.status);
+                  $scope.showSpinner = false;
+                });
+            } else {
+              $scope.getAllObservations();
+            }
+          };
 
     });
 })()
